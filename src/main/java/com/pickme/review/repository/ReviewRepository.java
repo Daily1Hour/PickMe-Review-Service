ad7b1e56484d947e3f7e833ca0cc4184179d497f
wlstn <wlstn159159@naver.com>
2024-11-25 20:25:18 +0900

    feat: ReviewRepository에 사용자 면접 리뷰 확인 및 조회 메서드 추가
    - existsByClientId(String clientId): 사용자의 면접 리뷰 존재 여부 확인
    - findByClientId(String clientId): 사용자의 면접 리뷰 정보 조회

--- src/main/java/com/pickme/review/repository/ReviewRepository.java
+++ src/main/java/com/pickme/review/repository/ReviewRepository.java
@@ -6,4 +6,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
+
+    // 해당 사용자의 저장된 면접 리뷰가 있는지 확인
+    boolean existsByClientId(String clientId);
+
+    // 해당 사용자 면접 리뷰 정보 추출
+    Review findByClientId(String clientId);
+
 }
