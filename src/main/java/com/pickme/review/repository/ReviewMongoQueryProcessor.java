587c04d9fda0a56fa755e06118ed120d74fd82fe
wlstn <wlstn159159@naver.com>
2024-11-26 17:38:47 +0900

    feat : ReviewMongoQueryProcessor에 면접 리뷰 삭제 기능 추가
    - findInterviewReview(Review review, String reviewId): reviewId에 해당하는 면접 리뷰 조회
    - deleteReview(Review review, String reviewId): 해당 면접 리뷰 삭제 및 변경된 review 저장
    - 면접 리뷰 삭제 후 성공 메시지를 반환

--- src/main/java/com/pickme/review/repository/ReviewMongoQueryProcessor.java
+++ src/main/java/com/pickme/review/repository/ReviewMongoQueryProcessor.java
@@ -1,14 +1,21 @@
 package com.pickme.review.repository;
 
 import com.pickme.review.entity.Review;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ReviewMongoQueryProcessor {
 
-    // 면접 리뷰 목록 가져오는 메서드
+    private final ReviewRepository reviewRepository;
+
+    // 필터링된 면접 리뷰 목록 가져오는 메서드
     public List<Review.InterviewReviews> filterFindInterviewReviews(Review review,
                                                                     String reviewId) {
 
@@ -17,5 +24,26 @@ public class ReviewMongoQueryProcessor {
                 .toList();
 
     }
+
+    // 사용자의 reviewId에 해당하는 일정 가져오는 메서드
+    public Review.InterviewReviews findInterviewReview(Review review, String reviewId) {
+        return review.getInterviewReviews().stream()
+                .filter(interviewReview -> interviewReview.getReviewId().equals(reviewId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public ResponseEntity<?> deleteReview(Review review, String reviewId) {
+        // reviewId에 해당하는 면접 리뷰를 갖고옴
+        Review.InterviewReviews interviewReview = findInterviewReview(review, reviewId);
+        // 해당 리뷰를 삭제
+        review.getInterviewReviews().remove(interviewReview);
+        // 변경된 review 저장
+        reviewRepository.save(review);
+
+        return ResponseEntity.status(HttpStatus.OK).body(String.format("%s 에 해당하는 면접 리뷰를 삭제했습니다.", reviewId));
+
+    }
+
 
 }
