12f1cd3e5080453a4d9632ec15fc226539d15b49
wlstn <wlstn159159@naver.com>
2024-12-09 21:39:27 +0900

    feat: HTTP 상태 코드에 대한 API 응답 및 설명 추가

--- src/main/java/com/pickme/review/controller/ReviewController.java
+++ src/main/java/com/pickme/review/controller/ReviewController.java
@@ -4,6 +4,9 @@ import com.pickme.review.dto.post.PostInterviewReviewsDTO;
 import com.pickme.review.dto.put.PutInterviewReviewsDTO;
 import com.pickme.review.service.ExternalApiService;
 import com.pickme.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +20,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/review")
 @Slf4j
+@Tag(name = "Review", description = "면접 리뷰(회고) API")
+@ApiResponse(responseCode = "400", description = "잘못된 요청")
+@ApiResponse(responseCode = "401", description = "권한 없음")
+@ApiResponse(responseCode = "403", description = "clientId에 해당하는 면접 일정 정보가 없음")
+@ApiResponse(responseCode = "404", description = "reviewId에 해당하는 면접 일정이 없음")
 public class ReviewController {
 
     private final ReviewService reviewService;
@@ -24,6 +32,8 @@ public class ReviewController {
     private final ExternalApiService externalApiService;
 
     // 면접 리뷰 생성&추가
+    @Operation(summary = "면접 리뷰 생성&추가", description = "면접 리뷰 생성&추가")
+    @ApiResponse(responseCode = "201", description = "면접 리뷰 생성&추가 성공")
     @PostMapping("/interview")
     public ResponseEntity<?> createReview(HttpServletRequest request,
                                           @RequestBody PostInterviewReviewsDTO postInterviewReviewsDTO) {
@@ -35,6 +45,8 @@ public class ReviewController {
     }
 
     // 면접 리뷰 조회
+    @Operation(summary = "면접 리뷰 조회", description = "면접 리뷰 전체 조회 & 특정 조건에 해당하는 면접 리뷰 조회")
+    @ApiResponse(responseCode = "200", description = "면접 리뷰 조회 요청 성공")
     @GetMapping("/interview")
     public ResponseEntity<?> findReview(HttpServletRequest request,
                                         @RequestParam(required = false) String reviewId) {
@@ -46,6 +58,8 @@ public class ReviewController {
     }
 
     // 면접 리뷰 삭제
+    @Operation(summary = "면접 리뷰 삭제", description = "reviewId에 해당하는 면접 리뷰 삭제")
+    @ApiResponse(responseCode = "200", description = "면접 리뷰 삭제 성공")
     @DeleteMapping("/interview")
     public ResponseEntity<?> deleteReview(HttpServletRequest request,
                                           @RequestParam String reviewId) {
@@ -56,6 +70,8 @@ public class ReviewController {
     }
 
     // 면접 리뷰 수정
+    @Operation(summary = "면접 리뷰 수정", description = "reviewId에 해당하는 면접 리뷰 수정")
+    @ApiResponse(responseCode = "200", description = "면접 리뷰 수정 성공")
     @PutMapping("/interview")
     public ResponseEntity<?> updateReview(HttpServletRequest request,
                                           @RequestParam String reviewId,
