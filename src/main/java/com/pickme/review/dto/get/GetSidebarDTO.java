2ba739ed61bcf2d2ed6266eb24cecdfc2f8546a6
wlstn <wlstn159159@naver.com>
2025-01-14 10:32:27 +0900

    feat: sidebar 조회 DTO 생성

---
+++ src/main/java/com/pickme/review/dto/get/GetSidebarDTO.java
@@ -0,0 +1,12 @@
+package com.pickme.review.dto.get;
+
+import lombok.Data;
+
+@Data
+public class GetSidebarDTO {
+    private String reviewId;
+
+    private String companyName;
+
+    private String category;
+}
